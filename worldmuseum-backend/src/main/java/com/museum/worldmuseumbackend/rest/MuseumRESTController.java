cf98bb459b38ff1dd816f718ff46bd2cbfd6dafd
BingNa Yang <bingnayang@gmail.com>
2020-12-05 14:32:40 -0500

    Create REST controller methods to GET by Id, POST, PUT, and DELETE

--- worldmuseum-backend/src/main/java/com/museum/worldmuseumbackend/rest/MuseumRESTController.java
+++ worldmuseum-backend/src/main/java/com/museum/worldmuseumbackend/rest/MuseumRESTController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.museum.worldmuseumbackend.entity.Museum;
@@ -12,6 +17,7 @@ import com.museum.worldmuseumbackend.service.MuseumService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MuseumRESTController {
 	private MuseumService museumService;
 	
@@ -22,9 +28,47 @@ public class MuseumRESTController {
 	}
 	
 	// GET "/museums" - return list of museums
-	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/museums")
-	public List<Museum> findAll(){
+	public List<Museum> findAllMuseum(){
 		return museumService.findAll();
+	}
+	
+	// GET "/museums/{museumId} - get museum by id
+	@GetMapping("/museums/{museumId}")
+	public Museum getMuseumById(@PathVariable int museumId) {
+		Museum theMuseum = museumService.findById(museumId);
+		
+		if(theMuseum == null) {
+			throw new RuntimeException("Museum Id not found: "+museumId);
+		}
+		
+		return theMuseum;
+	}
+	
+	// POST "/museums - add new museum 
+	@PostMapping("/museums")
+	public Museum saveMuseum(@RequestBody Museum theMuseum) {
+		theMuseum.setId(0);
+		museumService.save(theMuseum);
+		return theMuseum;
+	}
+	
+	// PUT "/museums" - update existing museum
+	@PutMapping("/museums")
+	public Museum updateMuseum(@RequestBody Museum theMuseum) {
+		museumService.save(theMuseum);
+		return theMuseum;
+	}
+	
+	// DELETE "/museums/{museumId}
+	@DeleteMapping("/museums/{museumId}")
+	public Museum deleteMuseumById(@PathVariable int museumId) {
+		Museum theMuseum = museumService.findById(museumId);
+		
+		if(theMuseum == null) {
+			throw new RuntimeException("Museum Id not found: "+museumId);
+		}
+		museumService.deleteById(museumId);
+		return theMuseum;
 	}
 }
