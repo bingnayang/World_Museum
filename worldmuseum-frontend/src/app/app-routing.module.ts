145de2b505d5df9087899e994c8cb1c28fa81873
BingNa Yang <bingnayang@gmail.com>
2020-12-05 13:44:37 -0500

    setup routing and navigation

--- worldmuseum-frontend/src/app/app-routing.module.ts
+++ worldmuseum-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MuseumListComponent } from './museum-list/museum-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'museums',component: MuseumListComponent},
+  {path: '',redirectTo: 'museums', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
