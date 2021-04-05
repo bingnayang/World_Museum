59e82e5d5fee6ee2b719a979238a1afbe9b4664c
BingNa Yang <bingnayang@gmail.com>
2021-04-05 17:53:21 -0400

    connecting to uploadImage and getImageList API

--- worldmuseum-frontend/src/app/services/image.service.ts
+++ worldmuseum-frontend/src/app/services/image.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Observable } from 'rxjs';
+import { Image } from '../image';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,4 +9,11 @@ export class ImageService {
   private baseURL = "http://localhost:8080/api/images";
 
   constructor(private httpClient: HttpClient) { }
+
+  getImageByMuseumId(id: number): Observable<Image[]>{
+    return this.httpClient.get<Image[]>(`${this.baseURL}/${id}`);
+  }
+  uploadImage(image: Image){
+    return this.httpClient.post<Image>(`${this.baseURL}`,image);
+  }
 }
